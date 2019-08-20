bf0139f7cb3e248a6823561f3fe83b5f09476bb1
ZhangZero1234 <1350706223@qq.com>
2019-08-20 22:29:14 +0800

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,35 @@
 <template>
-    <div id="card">
-        {{card}}
+    <div>
+            <cv-header aria-label="Carbon header">
+                    <cv-skip-to-content href="#main-content">
+                      Skip to content
+                    </cv-skip-to-content>
+                    <cv-header-name href="javascript:void(0)" prefix="IBM">
+                      [Platform]
+                    </cv-header-name>
+                      <template slot="left-panels" v-if="areLeftPanels">
+                        
+                      </template>
+                      <template slot="right-panels" v-if="areRightPanels">
+                      
+                    </template>
+                  </cv-header>
     </div>
-</template>
-<script>
+  </template>
+  
+  <script>
     export default {
-        data(){
-            return {
-                card:"CARD"
-            }
-        },
-        props:[]
+      data () {
+        return {
+          radio1: '上海',
+          radio2: '上海',
+          radio3: '上海',
+          radio4: '上海'
+        };
+      }
     }
-    
-</script>
+  </script>
+
 <style scoped lang="scss">
 
 </style>
