3226e3706aa11eedbfd4a75b11b0bc5690495679
ZhangZero1234 <1350706223@qq.com>
2019-08-20 22:33:21 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/styles/global.scss'
+import "carbon-components/scss/globals/scss/styles.scss" 
+import CarbonComponentsVue from '@carbon/vue/src/index';
+Vue.use(CarbonComponentsVue);
 console.log(App);
 let vm = new Vue({
     el:"#app",
